251212483ea795a42dd65da001ea7ea3c2f00943
Belial <100026757+ArulKunzzGT@users.noreply.github.com>
2024-05-21 17:59:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'flower', 'index.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'flower', 'index.html'));
 });
 
